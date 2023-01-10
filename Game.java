b5252bb2fcac4568093d2aff5d19c8405f574d3b
hnguyen <hnguyen@student-bmhs-la.org>
2023-01-10 09:10:46 -0800

    first practice commit

--- Game.java
+++ Game.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Game {
     private String hidden;
     private String guess;
-   // private String hint;
+    // private String hint;
 
     public static void main(String[]args){
         Game game = new Game();
@@ -20,38 +20,37 @@ public class Game {
         System.out.println("Welcome to Wordle!");
         System.out.println("A word with "+hidden.length()+" letters has been generated! Start your first guess");
 
-    while (runGame == true){
+        while (runGame == true){
 
-       guess = kb.next();
-       guess=guess.toLowerCase();
+            guess = kb.next();
+            guess=guess.toLowerCase();
 
-       if (guess.equals(hidden)){
-           System.out.print("Your guess is correct!");
-           runGame = false;
-        }
-        else{
-            for (int r = 0; r <hidden.length(); r++){
-                char ch = hidden.charAt(r);
-                if (guess.substring(r).equals(hidden.substring(r))){
-                         hint[r]= guess.charAt(r);
-               }
-                if (guess.indexOf(ch)==-1) {
-                    hint[r]='*';
-               }
-                    else if (guess.indexOf(ch)!=-1) {
-                    if (guess.charAt(r)==hidden.charAt(r)){
-                        hint[r]= guess.charAt(r);}
-                    else{
-                        hint[r]='+';}
-                }
-                System.out.print(hint[r]);
+            if (guess.equals(hidden)){
+                System.out.print("Your guess is correct!");
+                runGame = false;
             }
-            System.out.println();
-           System.out.println("Guess again!");
+            else{
+                for (int r = 0; r <hidden.length(); r++){
+                    char ch = guess.charAt(r);
+                    if (guess.substring(r).equals(hidden.substring(r))){
+                        hint[r]= guess.charAt(r);
+                    }
+                    if (hidden.indexOf(ch)==-1) {
+                        hint[r]='*';
+                    }
+                    else if (hidden.indexOf(ch)!=-1) {
+                        if (guess.charAt(r)==hidden.charAt(r)){
+                            hint[r]= guess.charAt(r);}
+                        else{
+                            hint[r]='+';}
+                    }
+                    System.out.print(hint[r]);
+                }
+                System.out.println();
+                System.out.println("Guess again!");
+
+            }
 
         }
-
-    }
 
     }}
-
